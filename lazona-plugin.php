875a582e9b96eaa29463ca8490b23f4a72027a37
David Igón <davidbenabarre@platoniq.net>
2021-03-31 12:19:04 +0200

    first definition of headers for the plugin

---
+++ lazona-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ Plugin Name: Goteo LaZona plugin
+ Plugin URI: https://git.goteo.org/dev/la-zona
+ Description: This Wordpress - Woocommerce plugin connects a marketplace to the crowdfunding platform Goteo.org
+ Version: 0.0.1
+ Author: David Benabarre
+ License: GNU Public License v3
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+   die;
+ }
