9c1ada4016bf4f2ca7bd7153ea9e52fa1dc5eb9c
David Igón <david.igon@riseup.net>
2021-04-06 15:57:17 +0200

    add new plugin headers

--- lazona-plugin.php
+++ lazona-plugin.php
@@ -4,7 +4,8 @@
  Plugin URI: https://git.goteo.org/dev/la-zona
  Description: This Wordpress - Woocommerce plugin connects a marketplace to the crowdfunding platform Goteo.org
  Version: 0.0.1
- Author: David Benabarre
+ Author: Platoniq Foundation, Goteo
+ Author URI: https://goteo.org
  License: GNU Public License v3
  */
 
