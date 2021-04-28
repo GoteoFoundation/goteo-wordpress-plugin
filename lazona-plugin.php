f5c4bc033a133f03dfa960f288189d96420551ed
David Igón <davidbenabarre@platoniq.net>
2021-04-28 18:46:29 +0200

    add new options to the plugin and to the form in the admin template

--- lazona-plugin.php
+++ lazona-plugin.php
@@ -7,6 +7,7 @@
  Author: Platoniq Foundation, Goteo
  Author URI: https://goteo.org
  License: GNU Public License v3
+ Domain: /languages
  */
 
  if ( ! defined( 'ABSPATH' ) ) {
@@ -38,17 +39,113 @@
     add_action('admin_enqueue_scripts', array($this, 'enqueue'));
     // add_action('wq_enqueue_scripts', array($this, 'enqueue'));
 
-    add_action('admin_menu', array($this, 'add_admin_pages'));
+    add_action( 'admin_menu' , array($this, 'add_admin_pages'));
 
-    add_filter("plugin_action_links_" . $this->plugin, array( $this, 'settings_link'));
+    add_filter( 'plugin_action_links_' . $this->plugin, array( $this, 'settings_link'));
+
+    add_action( 'admin_init', array($this, 'my_settings_init'));
+
+    add_action( 'admin_init', array($this, 'goteo_api_init'));
   }
 
+  public function my_settings_init() {
+    add_settings_section(
+      'sample_page_setting_section',
+      __( 'Goteo Plugin Settings', 'goteo-admin-setting' ),
+      array($this, 'comission_callback_function'),
+      'goteo-settings'
+    );
+
+    add_settings_field(
+      'goteo_comission',
+      __( 'Comisión', 'goteo-comission' ),
+      array($this, 'my_setting_markup'),
+      'goteo-settings',
+      'sample_page_setting_section'
+    );
+
+    add_settings_field(
+      'goteo_date',
+      __( 'Fecha', 'goteo-date-definition'),
+      array($this, 'date_definition_markup'),
+      'goteo-settings',
+      'sample_page_setting_section'
+    );
+
+    register_setting( 'goteo-settings', 'goteo_comission' );
+    register_setting( 'goteo-settings', 'goteo_date');
+
+
+  }
+
+  function goteo_api_init() {
+    add_settings_section(
+      'goteo_page_api_section',
+      __( 'Goteo API KEY Settings', 'goteo-admin-setting' ),
+      array($this, 'apikey_callback_function'),
+      'goteo-apikey'
+    );
+
+    add_settings_field(
+      'goteo_user',
+      __( 'User', 'goteo-user' ),
+      array($this, 'goteo_user_markup'),
+      'goteo-apikey',
+      'goteo_page_api_section'
+    );
+
+    add_settings_field(
+      'goteo_key',
+      __( 'Key', 'goteo-key'),
+      array($this, 'goteo_apikey_markup'),
+      'goteo-apikey',
+      'goteo_page_api_section'
+    );
+
+    register_setting( 'goteo-apikey', 'goteo_user' );
+    register_setting( 'goteo-apikey', 'goteo_key');
+  }
+
+  function date_definition_markup() {
+    ?>
+    <!-- <label for="goteo_comission"><?php _e( 'Comisión', 'goteo-admin-setting-comission' ); ?></label> -->
+    <input type="date" id="goteo_date" name="goteo_date" value="<?php echo get_option( 'goteo_date' ); ?>" required>
+    <?php
+  }
+
+  function my_setting_markup() {
+    ?>
+    <!-- <label for="goteo_comission"><?php _e( 'Comisión', 'goteo-admin-setting-comission' ); ?></label> -->
+    <input type="number" id="goteo_comission" name="goteo_comission" step="0.1" value="<?php echo get_option( 'goteo_comission' ); ?>" required>
+    <?php
+  }
+
+  function goteo_apikey_markup() {
+    ?>
+    <input type="text" id="goteo_key" name="goteo_key" value="<?php echo get_option( 'goteo_key' ); ?>" required>
+    <?php
+
+  }
+
+  function goteo_user_markup() {
+    ?>
+    <input type="text" id="goteo_user" name="goteo_user" value="<?php echo get_option( 'goteo_user' ); ?>" required>
+    <?php
+  }
+
+
+  function comission_callback_function( $args ) {
+    echo '<p>El % de comisión que escojas se utilizará para calcular la cantidad de dinero que puedes donar a través de <a href="https://goteo.org">Goteo.org</a></p>';
+  }
+  
+
   public function settings_link($links) {
-    $settings_link = '<a href="admin.php?page=goteo_lazona_plugin">Settigns</a>';
+    $settings_link = '<a href="admin.php?page=goteo_lazona_plugin">Settings</a>';
     array_push($links, $settings_link);
     return $links;
   }
 
+  // deals with the routing of the admin menu to the admin_index
   public function add_admin_pages() {
     add_menu_page('Crowdfunding Plugin', 'Crowdfunding', 'manage_options', 'goteo_lazona_plugin', array($this, 'admin_index'), plugins_url('/assets/icon/goteo.svg', __FILE__), 110);
   }
@@ -59,10 +156,12 @@
 
   public function enqueue() {
     wp_enqueue_style('goteo_styles', plugins_url('/assets/goteo_styles.css', __FILE__));
-    wp_enqueue_script('goteo_scripts', plugins_url('/assets/goteo_javascript.js', __FILE__));
+    wp_enqueue_script('gotoe_javascript', plugins_url('/assets/goteo_javascript.js', __FILE__));
+    wp_enqueue_script('goteo_api', plugins_url('/assets/goteo_api.js', __FILE__));
   }
 
 }
+
 
 if ( class_exists('GoteoLaZona')) {
   $goteoLaZona = new GoteoLaZona();
--- templates/admin/admin.php
+++ templates/admin/admin.php
@@ -1,1 +1,62 @@
-<h1> Admin Goteo Plugin </h1>
+<!-- <h1> Admin Goteo Plugin </h1> -->
+
+<div class="wrap">
+  <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+  <form action="options.php" method="post">
+      <?php
+      // output security fields for the registered setting "wporg"
+        settings_fields( 'goteo-settings' );
+        // output setting sections and their fields
+        // (sections are registered for "goteo-settings", each field is registered to a specific section)
+        do_settings_sections( 'goteo-settings' );
+        // output save settings button
+        submit_button( 'Save Settings' );
+      ?>
+  </form>
+</div>
+
+<div class="wrap">
+  <form action="options.php" method="GET" onsubmit="checkApiKey(event)">
+
+    <?php
+
+      $valid_auth = false;
+      $curl = curl_init();
+
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.goteo.org/v1/login",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_USERPWD => get_option('goteo_user') . ":" . get_option('goteo_key'),
+        CURLOPT_HTTPAUTH, CURLAUTH_BASIC
+      ));
+
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
+
+      curl_close($curl);
+
+      if ($err) {
+        echo "cURL Error #:" . $err;
+      } else {
+        $valid_auth = true;
+        $response_json = json_decode($response);
+        add_option( 'goteo_token', $response_json->access_token);
+      }
+
+    ?>
+    <div id="infoMessages" class=""></div>
+
+    <?php
+      settings_fields( 'goteo-apikey' );
+      // output setting sections and their fields
+      // (sections are registered for "goteo-settings", each field is registered to a specific section)
+      do_settings_sections( 'goteo-apikey' );
+      // output save settings button
+
+      submit_button( 'Save API Credentials');
+
+      submit_button( 'Check API' , 'primary', 'check_api');
+      ?>
+  </form>
+<div>
