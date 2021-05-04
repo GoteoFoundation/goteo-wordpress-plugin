89e687dd9caa3c36ce000e3fba1369f4ae991d29
David Igón <davidbenabarre@platoniq.net>
2021-05-04 16:14:06 +0200

    add new sections to admin module and change connection to goteo api to be done in the backend

--- templates/admin/admin.php
+++ templates/admin/admin.php
@@ -1,8 +1,9 @@
 <!-- <h1> Admin Goteo Plugin </h1> -->
 
+<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+
 <div class="wrap">
-  <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-  <form action="options.php" method="post">
+  <form action="options.php" method="POST">
       <?php
       // output security fields for the registered setting "wporg"
         settings_fields( 'goteo-settings' );
@@ -16,36 +17,40 @@
 </div>
 
 <div class="wrap">
-  <form action="options.php" method="GET" onsubmit="checkApiKey(event)">
+  <form action="options.php" method="POST">
 
     <?php
 
-      $valid_auth = false;
-      $curl = curl_init();
+      $args = array(
+        'headers' => array(
+          'Authorization' => 'Basic ' . base64_encode( get_option('goteo_user') . ":" . get_option('goteo_key') )
+          )
+        );
 
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.goteo.org/v1/login",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_USERPWD => get_option('goteo_user') . ":" . get_option('goteo_key'),
-        CURLOPT_HTTPAUTH, CURLAUTH_BASIC
-      ));
+      $response = wp_remote_get(
+        'https://api.goteo.org/v1/login',
+        $args
+      );
 
-      $response = curl_exec($curl);
-      $err = curl_error($curl);
+      $http_code = wp_remote_retrieve_response_code($response);
 
-      curl_close($curl);
-
-      if ($err) {
-        echo "cURL Error #:" . $err;
+      if ($http_code == 200) {
+        $access_token = json_decode(wp_remote_retrieve_body($response))->access_token;
+        add_option('goteo_token', $access_token);
       } else {
-        $valid_auth = true;
-        $response_json = json_decode($response);
-        add_option( 'goteo_token', $response_json->access_token);
+        add_option('goteo_token', null);
       }
 
     ?>
-    <div id="infoMessages" class=""></div>
+    <div id="infoMessages" class="<?= (get_option('goteo_token'))? "notice notice-success": "notice notice-error"; ?>">
+      <?php
+        if (get_option('goteo_token')):
+          echo "Success autenticating";
+        else:
+          echo "Error autenticating";
+        endif;
+      ?>
+    </div>
 
     <?php
       settings_fields( 'goteo-apikey' );
@@ -56,7 +61,71 @@
 
       submit_button( 'Save API Credentials');
 
-      submit_button( 'Check API' , 'primary', 'check_api');
+      // submit_button( 'Check API' , 'primary', 'check_api');
       ?>
   </form>
 <div>
+
+
+<div class="wrap">
+
+  <h1> Saldo comprometido </h1>
+
+  <div class="">
+        <?php
+          // TODO: Integrate with the WooCommerce API
+        ?>
+  </div>
+
+</div>
+
+<div class="wrap">
+
+  <h1> Saldo enviado a Goteo </h1>
+
+  <div class="">
+        <?php
+
+        ?>
+  </div>
+  
+</div>
+
+<div class="wrap">
+
+  <h1> Saldo pendiente de enviar </h1>
+
+  <div class="">
+        <?php
+
+        ?>
+  </div>
+  
+</div>
+
+<div class="wrap">
+
+  <h1> Proyectos beneficiarios </h1>
+
+  <div class="">
+    <?php
+      $projects_response = wp_remote_get(
+        'https://api.goteo.org/v1/matchers/' . get_option('goteo_user'). '/projects',
+        $args
+      );
+
+      $projects = json_decode(wp_remote_retrieve_body($projects_response));
+
+    ?>
+    <div class="goteo-project-mosaic">
+    <?php
+      foreach ($projects->items as $project) :
+    ?>
+      <iframe frameborder="0" height="492px" src="//ca.goteo.org/widget/project/<?= $project->id ?>?lang=ca" width="300px" scrolling="no"></iframe>
+    <?php
+      endforeach;
+    ?>
+    </div>
+  </div>
+  
+</div>
